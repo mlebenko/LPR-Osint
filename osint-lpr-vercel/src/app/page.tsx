c141cbdef5a1f05d96ee9b3e8588cfb043ac68a1
mlebenko <istvein@gmail.com>
2025-08-26 17:31:58 +0300

    Update page.tsx

--- osint-lpr-vercel/src/app/page.tsx
+++ osint-lpr-vercel/src/app/page.tsx
@@ -195,8 +195,8 @@ function mdToHtml(md: string) {
     .replace(/^## (.*$)/gim, "<h2>$1</h2>")
     .replace(/^# (.*$)/gim, "<h1>$1</h1>")
     .replace(/\*\*(.*?)\*\*/gim, "<b>$1</b>")
-    .replace(/\[(.*?)\]\((.*?)\)/gim, '<a href="$2" target="_blank">$1</a>')
+    .replace(/\[(.*?)\]\((.*?)\)/gim, '<a href="$2" target="_blank" rel="noreferrer">$1</a>')
     .replace(/^\- (.*$)/gim, "<li>$1</li>")
-    .replace(/
-/g, "<br/>");
+    .split("\n").join("<br/>");
 }
+
